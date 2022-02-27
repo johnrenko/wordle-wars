172ee3d985815b1a3e8b95c6b37fe21afa5843e8
John Dutamby <johnrenko@users.noreply.github.com>
2022-02-27 10:41:32 +0100

    Change keyboard to azerty

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -10,9 +10,9 @@ defineEmits<{
 }>()
 
 const rows = [
-  'qwertyuiop'.split(''),
-  'asdfghjkl'.split(''),
-  ['Enter', ...'zxcvbnm'.split(''), 'Backspace']
+  'azertyuiop'.split(''),
+  'qsdfghjklm'.split(''),
+  ['Enter', ...'wxcvbn'.split(''), 'Backspace']
 ]
 </script>
 
